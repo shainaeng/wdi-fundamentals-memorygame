c12f139a8cc8fdaae4ecb6b188027e2cdea6b4f0
Shaina <shaina.eng@gmail.com>
2018-02-06 08:53:16 -0800

    Adding JS Unit 9 Assignment work

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -2,16 +2,27 @@ var cards = ["queen", "queen", "king", "king"];
 
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped " + cardOne);
+var checkForMatch = function () {
+    if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
+      alert("You found a match!");
+    } else if (cardsInPlay.length === 2 && cardsInPlay[0] !== cardsInPlay[1]) {
+      alert("Sorry, try again.");
+    }
+// This is the GA-provided code for the above lines (but it logs a message after each flip):
+    //if (cardsInPlay[0] === cardsInPlay[1]) {
+      //console.log("You found a match!");
+    //} else {
+      //console.log("Sorry, try again.");
+    //}
+};
 
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardTwo);
+var flipCard = function (cardId) {
+  console.log("User flipped " + cards[cardId]);
+  cardsInPlay.push(cards[cardId]);
 
-if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
-  alert("You found a match!");
-} else if (cardsInPlay.length === 2 && cardsInPlay[0] !== cardsInPlay[1]) {
-  alert("Sorry, try again.");
-}
+
+checkForMatch();
+};
+
+flipCard(0);
+flipCard(2);

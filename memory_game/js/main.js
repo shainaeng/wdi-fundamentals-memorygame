911c952e3ba08f3acf6b2b7cc57e7d65136fcde5
Shaina <shaina.eng@gmail.com>
2018-02-20 20:19:28 -0800

    Unit 10 Additions

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,4 +1,25 @@
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+{
+    rank: "queen",
+    suit: "hearts",
+    cardImage: "images/queen-of-hearts.png"
+},
+{
+    rank: "queen",
+    suit: "diamonds",
+    cardImage: "images/queen-of-diamonds.png"
+},
+{
+    rank: "king",
+    suit: "hearts",
+    cardImage: "images/king-of-hearts.png"
+},
+{
+    rank: "king",
+    suit: "diamonds",
+    cardImage: "images/king-of-diamonds.png"
+}
+];
 
 var cardsInPlay = [];
 
@@ -8,17 +29,13 @@ var checkForMatch = function () {
     } else if (cardsInPlay.length === 2 && cardsInPlay[0] !== cardsInPlay[1]) {
       alert("Sorry, try again.");
     }
-// This is the GA-provided code for the above lines (but it logs a message after each flip):
-    //if (cardsInPlay[0] === cardsInPlay[1]) {
-      //console.log("You found a match!");
-    //} else {
-      //console.log("Sorry, try again.");
-    //}
 };
 
 var flipCard = function (cardId) {
-  console.log("User flipped " + cards[cardId]);
-  cardsInPlay.push(cards[cardId]);
+  console.log("User flipped " + cards[cardId].rank);
+  cardsInPlay.push(cards[cardId].rank);
+  console.log(cards[cardId].cardImage);
+  console.log(cards[cardId].suit);
 
 
 checkForMatch();

4dac2a3748ec409d93f5250675446f59ba7c71f5
Shaina <shaina.eng@gmail.com>
2018-02-23 13:11:39 -0800

    Struggling through Unit 11

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -24,6 +24,7 @@ var cards = [
 var cardsInPlay = [];
 
 var checkForMatch = function () {
+  this.setAttribute("src", cards[cardId].cardImage);
     if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
       alert("You found a match!");
     } else if (cardsInPlay.length === 2 && cardsInPlay[0] !== cardsInPlay[1]) {
@@ -31,7 +32,8 @@ var checkForMatch = function () {
     }
 };
 
-var flipCard = function (cardId) {
+var flipCard = function () {
+  var cardId = this.getAttribute("data-id");
   console.log("User flipped " + cards[cardId].rank);
   cardsInPlay.push(cards[cardId].rank);
   console.log(cards[cardId].cardImage);
@@ -41,5 +43,14 @@ var flipCard = function (cardId) {
 checkForMatch();
 };
 
-flipCard(0);
-flipCard(2);
+var createBoard = function () {
+  for (var i = 0; i < cards.length; i++) {
+    var cardElement = document.createElement("img");
+    cardElement.setAttribute("src", "images/back.png");
+    cardElement.setAttribute("data-id", i);
+    cardElement.addEventListener("click", "flipCard");
+    document.getElementById("game-board").appendChild(cardElement);
+  }
+};
+
+createBoard();
